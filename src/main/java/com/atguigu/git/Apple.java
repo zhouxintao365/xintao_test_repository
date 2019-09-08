62d397fb6d28f43a7cea2ee53e6213df27f54aff
zhouxintao365 <zhouxintao365@aliyun.com>
2019-09-08 18:16:10 +0800

    modified on branch hotFix

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -4,5 +4,6 @@ public class Apple {
 	public static void main(String[] args) {
 		System.out.println("eclipse modified..");
 		System.out.println("apple...");
+		System.out.println("branch hotFix modified");
 	}
 }
